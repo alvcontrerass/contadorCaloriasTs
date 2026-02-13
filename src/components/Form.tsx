57e76a41cbf9202bf783a67c9c7b8b54bc927624
alvcontrerass <alv.contrerass@gmail.com>
2026-02-13 18:21:07 -0300

    Añadir estado para la gestión de actividad en el formulario. Cambio de nombre de activity por name para evitar confuciones con state

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,11 +1,19 @@
 import {categories} from "../data/categories"
+import { useState } from "react"
 
 export default function Form() {
+
+    const [activity, setActivity] = useState({
+        category: 1,
+        name: '',
+        calories: 0
+    })
+
   return (
     <form action="" className="space-y-5 bg-white shadow p-10 rounded-lg">
         <div className="grid grid-cols-1 gap-3">
             <label htmlFor="category" className="font-bold">Categoria:</label>
-            <select name="category" id="category" className=" border border-slate-300 p-2 rounded-lg w-full bg-white">
+            <select name="category" id="category" className=" border border-slate-300 p-2 rounded-lg w-full bg-white" value={activity.category}>
                 {categories.map(category => (
                     <option value={category.id} key={category.id}>
                         {category.name}
@@ -15,13 +23,13 @@ export default function Form() {
         </div>
 
         <div className="grid grid-cols-1 gap-3">
-            <label htmlFor="activity" className="font-bold">Actividad:</label>
-            <input type="text" id="activity" className="border border-slate-300 p-2 rounded-lg" placeholder="Ej. Comida, Jugo de naranja, Ejercicio, Pesas, Caminata"/>
+            <label htmlFor="name" className="font-bold">Actividad:</label>
+            <input type="text" id="name" className="border border-slate-300 p-2 rounded-lg" placeholder="Ej. Comida, Jugo de naranja, Ejercicio, Pesas, Caminata" value={activity.name}/>
         </div>
 
         <div className="grid grid-cols-1 gap-3">
             <label htmlFor="calories" className="font-bold">Calorias:</label>
-            <input type="number" id="calories" className="border border-slate-300 p-2 rounded-lg" placeholder="Cantidad de calorias"/>
+            <input type="number" id="calories" className="border border-slate-300 p-2 rounded-lg" placeholder="Cantidad de calorias" value={activity.calories}/>
         </div>
 
         <input type="submit" className="bg-gray-800 hover:bg-gray-900 w-full p-2 font-bold uppercase text-white cursor-pointer" value="Guardar Comida o Ejercicio" />
